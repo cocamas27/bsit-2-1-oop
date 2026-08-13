2dff1eaa7007a357f9fd0c869db14276a2720a41
cocamas27 <cocamasa98527@liceo.edu.ph>
2026-08-13 09:29:04 +0800

    lab Activity 6

---
+++ src/Motorcycle.java
@@ -0,0 +1,15 @@
+public class Motorcycle extends vehicle {
+
+    private boolean hasSidecar;
+
+    public Motorcycle(String brand, int year, boolean hasSidecar) {
+        super(brand, year);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        String sidecarText = hasSidecar ? "with sidecar" : "no sidecar";
+        System.out.println("Motorcycle: " + brand + " (" + year + ") - " + sidecarText);
+    }
+}
